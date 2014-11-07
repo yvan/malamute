4b2cc8cf391ea34e0e6b3e5a3fa95707fc309e02
Quique Lores <quique.lores@gmail.com>
2014-11-07 14:20:04 -0500

    file system testing

--- malamuteTests/filesystemTests.m
+++ malamuteTests/filesystemTests.m
@@ -8,27 +8,46 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <MultipeerConnectivity/MultipeerConnectivity.h>
 
-@interface filesystemTests : XCTestCase
+#import "ViewController.h"
 
+@interface filesystemTests : XCTestCase{
+
+    UIApplication* app;
+    ViewController* viewController;
+    MCPeerID* testPeer;
+}
 @end
 
 @implementation filesystemTests
 
 - (void)setUp {
     [super setUp];
+    app = [UIApplication sharedApplication];
+    viewController = [[ViewController alloc] init];
+    [viewController viewDidLoad];
+    testPeer = [[MCPeerID alloc] initWithDisplayName:@"testPeerID"];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    app = nil;
+    viewController = nil;
+    testPeer = nil;
 }
 
 - (void)testExample {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
 }
+
+/*-(void)testReceivingResource{
+    [viewController didFinishReceivingResource:viewController.sessionWrapper.session resourceName:@"Something.jpeg" fromPeer:testPeer atURL:[[NSURL alloc] initFileURLWithPath:@"malamute/docs/test/" isDirectory:YES ] withError:nil];
+    XCTAssert(YES, @"I don't know");
+}*/
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
