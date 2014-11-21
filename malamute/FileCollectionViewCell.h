7072d3248991bc1ae5f471f1b8763059af4c063c
Ivan <yvanscher@gmail.com>
2014-11-21 18:54:05 -0500

    pulling

---
+++ malamute/FileCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FileCellCollectionViewCell.h
+//  malamute
+//
+//  Created by Yvan Scher on 11/21/14.
+//  Copyright (c) 2014 Yvan Scher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FileCellCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ malamute/FileCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  FileCellCollectionViewCell.m
+//  malamute
+//
+//  Created by Yvan Scher on 11/21/14.
+//  Copyright (c) 2014 Yvan Scher. All rights reserved.
+//
+
+#import "FileCellCollectionViewCell.h"
+
+@implementation FileCellCollectionViewCell
+
+@end
