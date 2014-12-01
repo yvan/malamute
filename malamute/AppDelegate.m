a5a6e20927f285411ac1f0234d8d187e6df643b0
Ivan <yvanscher@gmail.com>
2014-12-01 16:23:21 -0500

    we should now be saving our filesystem on crash or entering the background.

--- malamute/AppDelegate.m
+++ malamute/AppDelegate.m
@@ -28,7 +28,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
+    [_viewController.fileSystem saveFileSystemToJSON]; // this needs to run in 5 sec or under to work.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -41,6 +41,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [_viewController.fileSystem saveFileSystemToJSON]; // this needs to run in 5 sec or under to work?
 }
 
 @end
